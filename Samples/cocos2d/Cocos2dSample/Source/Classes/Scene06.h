56306bcdd8c8965e6d943861de988203ca2b5e43
anton-nikan <anton.nikan@gmail.com>
2011-03-01 12:09:12 +0200

    - Tag element implemented: allows to respond on different parts of animation clip. Sample Scene 06. - Single Frame element allowed instead of Frames list. - RandomSpawn element is now an alias for RandomItem.

---
+++ Samples/cocos2d/Cocos2dSample/Source/Classes/Scene06.h
@@ -0,0 +1,16 @@
+//
+//  Scene06.h
+//  Cocos2dSample
+//
+//  Created by nikan on 3/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Scene06 : SceneBase {
+    
+}
+
+@end
---
+++ Samples/cocos2d/Cocos2dSample/Source/Classes/Scene06.m
@@ -0,0 +1,14 @@
+//
+//  Scene06.m
+//  Cocos2dSample
+//
+//  Created by nikan on 3/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Scene06.h"
+
+
+@implementation Scene06
+
+@end
